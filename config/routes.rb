852a87ee6f50831d68e8ff1b5242a9eece74d024
Alex Covarrubias <kuhruby@gmail.com>
2014-08-19 21:24:59 -0500

    add request form

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
 
   root to: 'home#index'
   get '/staff' => 'home#staff', as: :staff
+  get '/contacts' => 'contacts#new', as: :contacts
+  resources "contacts", only: [:new, :create]
 
 end
